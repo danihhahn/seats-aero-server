54e61423f2cbdda42e0d8504ba6aa2985b3cf6c3
danihhahn <danihhahn@gmail.com>
2025-03-16 14:13:46 -0300

    Correção do endpoint da API do Seats.aero

--- app.py
+++ app.py
@@ -22,17 +22,18 @@ def buscar_voos():
     if not origem or not destino or not data:
         return jsonify({"erro": "Parâmetros inválidos. Use origem, destino e data."}), 400
     
-    url = f"https://seats.aero/partnerapi/search?origin={origem}&destination={destino}&date={data}"
+    url = f"https://seats.aero/partnerapi/search?origin_airport={origem}&destination_airport={destino}&start_date={data}"
     headers = {
-    "Partner-Authorization": "pro_2mYiRPo4mnGgDsp8RO4tGVSvH4a",
-    "accept": "application/json"}
+        "Partner-Authorization": f"{SEATS_AERO_API_KEY}",
+        "Accept": "application/json"
+    }
     
     response = requests.get(url, headers=headers)
     
     if response.status_code == 200:
         return jsonify(response.json())
     else:
-        return jsonify({"erro": f"Erro na requisição: {response.status_code}"})
+        return jsonify({"erro": f"Erro na requisição: {response.status_code}", "detalhes": response.text})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
